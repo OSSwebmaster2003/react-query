443de1909797910c3bfaa78ccff3553668d3eeb6
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-04-10 02:55:31 -0700

    handling query error

--- src/components/Superheroes.page.js
+++ src/components/Superheroes.page.js
@@ -4,16 +4,26 @@ import { useEffect, useState } from "react";
 function Superheroes_page(props) {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      setData(res.data);
-      setLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
   }, []);
 
   if (loading) {
     return <h2>Loading ...</h2>;
+  }
+  if (error) {
+    return <h2>{error}</h2>;
   }
   return (
     <div>
